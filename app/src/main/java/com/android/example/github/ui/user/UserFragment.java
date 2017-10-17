e792ac61fd16d2bf7f8aa426ae70a59932ed4033
gsipic <goran.sipic87@gmail.com>
2017-10-17 19:58:02 +0200

    detail repo #5

--- app/src/main/java/com/android/example/github/ui/user/UserFragment.java
+++ app/src/main/java/com/android/example/github/ui/user/UserFragment.java
@@ -8,6 +8,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -49,6 +50,7 @@ public class UserFragment extends LifecycleFragment implements Injectable {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        navigationController = new NavigationController((AppCompatActivity) getActivity());
         UserFragmentBinding dataBinding = DataBindingUtil.inflate(inflater, R.layout.user_fragment,
                 container, false, dataBindingComponent);
         dataBinding.setRetryCallback(() -> userViewModel.retry());
